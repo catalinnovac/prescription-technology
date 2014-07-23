c020b7671624dfd6eb252141a9bda2b7a9d71d0d
catalinnovac <novaccatalin90@hotmail.com>
2014-07-23 19:08:05 +0300

    remove activities

--- src/prescription/technology/code/Constants.java
+++ src/prescription/technology/code/Constants.java
@@ -1,8 +1,32 @@
 package prescription.technology.code;
 
+import android.webkit.JavascriptInterface;
+
 public class Constants {
-    public static String CUSTOMER_ADDRESS_ACTIVITY = "prescription.technology.CUSTOMER_ADDRESS";
-    public static String CUSTOMER_PERSONAL_INFO_ACTIVITY = "prescription.technology.CUSTOMER_PERSONAL_INFO";
-    public static String ORDER_CONFIRMARTION_ACTIVITY = "prescription.technology.ORDER_CONFIRMATION";
-    public static String QUESTIONS_ACTIVITY = "prescription.technology.QUESTIONS";
+
+
+    public final String CUSTOMER_ADDRESS_ACTIVITY = "prescription.technology.CUSTOMER_ADDRESS";
+    public final String CUSTOMER_PERSONAL_INFO_ACTIVITY = "prescription.technology.CUSTOMER_PERSONAL_INFO";
+    public final String ORDER_CONFIRMARTION_ACTIVITY = "prescription.technology.ORDER_CONFIRMATION";
+    public final String QUESTIONS_ACTIVITY = "prescription.technology.QUESTIONS";
+
+    @JavascriptInterface
+    public final String getCUSTOMER_ADDRESS_ACTIVITY() {
+        return CUSTOMER_ADDRESS_ACTIVITY;
+    }
+
+    @JavascriptInterface
+    public final String getCUSTOMER_PERSONAL_INFO_ACTIVITY() {
+        return CUSTOMER_PERSONAL_INFO_ACTIVITY;
+    }
+
+    @JavascriptInterface
+    public final String getORDER_CONFIRMARTION_ACTIVITY() {
+        return ORDER_CONFIRMARTION_ACTIVITY;
+    }
+
+    @JavascriptInterface
+    public final String getQUESTIONS_ACTIVITY() {
+        return QUESTIONS_ACTIVITY;
+    }
 }
